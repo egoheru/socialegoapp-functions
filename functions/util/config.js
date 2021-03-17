8bc786f2e133b2dcdce7c61907d454e113598733
egoheru <egoherokugit@gmail.com>
2021-03-17 12:13:49 +0100

    updated

---
+++ functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyB2cJv18dbLeJ18zUc5GNx_xMQYp8ZS6ik",
+    authDomain: "socialappe-72b1d.firebaseapp.com",
+    projectId: "socialappe-72b1d",
+    storageBucket: "socialappe-72b1d.appspot.com",
+    messagingSenderId: "739387541583",
+    appId: "1:739387541583:web:fc663ac6ab613f559b423a"
+  };
